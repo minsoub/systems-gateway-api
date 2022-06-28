3a9f9d9c258fe8a43bf50866662e90c0921a14a2
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-06-28 16:39:33 +0900

    audit log 작업 - 개발 sqs 연동

--- src/main/java/com/bithumbsystems/SystemsApiGatewayApplication.java
+++ src/main/java/com/bithumbsystems/SystemsApiGatewayApplication.java
@@ -3,9 +3,8 @@ package com.bithumbsystems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@ConfigurationPropertiesScan("com.bithumbsystems.config.properties.UrlConfig")
+@ConfigurationPropertiesScan("com.bithumbsystems.config")
 @SpringBootApplication
 public class SystemsApiGatewayApplication {
     public static void main(String[] args) {
