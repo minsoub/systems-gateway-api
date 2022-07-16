93384707d0d923b74887c1a17ccfc7fb56c43516
seongjinna <seongjinna@gmail.com>
2022-07-16 17:52:34 +0900

    HttpClient connection fifo -> lifo 변경 테스트

--- src/main/java/com/bithumbsystems/filter/ApiFilter.java
+++ src/main/java/com/bithumbsystems/filter/ApiFilter.java
@@ -65,7 +65,9 @@ public class ApiFilter extends AbstractGatewayFilterFactory<Config> {
         .maxIdleTime(Duration.ofSeconds(20))
         .maxLifeTime(Duration.ofSeconds(600))
         .pendingAcquireTimeout(Duration.ofSeconds(60))
-        .evictInBackground(Duration.ofSeconds(120)).build();
+        .evictInBackground(Duration.ofSeconds(120))
+        .lifo()
+        .build();
 
     HttpClient httpClient = HttpClient.create(provider)
         .doOnConnected(
