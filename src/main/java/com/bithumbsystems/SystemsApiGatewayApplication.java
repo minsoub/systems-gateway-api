30a117bb3ca475001256c7e112bc80f0a1786f3e
seongjinna <seongjinna@gmail.com>
2022-08-19 16:26:27 +0900

    PID 설정 추가

--- src/main/java/com/bithumbsystems/SystemsApiGatewayApplication.java
+++ src/main/java/com/bithumbsystems/SystemsApiGatewayApplication.java
@@ -2,12 +2,15 @@ package com.bithumbsystems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan("com.bithumbsystems.config")
 @SpringBootApplication
 public class SystemsApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SystemsApiGatewayApplication.class, args);
+        SpringApplication app = new SpringApplication(SystemsApiGatewayApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 }
